178fb0ce01009533c5de6c4079025e64f14d5565
Yg0R2 <yg0r2@yahoo.com>
2017-09-29 09:41:12 +0200

    Move mod specific event registration into the mod instance

--- core/src/main/java/yg0r2/extras/core/McExtrasCoreMod.java
+++ core/src/main/java/yg0r2/extras/core/McExtrasCoreMod.java
@@ -1,12 +1,6 @@
 package yg0r2.extras.core;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import yg0r2.extras.core.blocks.BlockRegistry;
-import yg0r2.extras.core.items.ItemRegistry;
-import yg0r2.extras.core.recipes.RecipeRegistry;
-import yg0r2.extras.core.world.WorldGeneratorRegistry;
 
 @Mod(name = McExtrasCoreMod.MOD_NAME, modid = McExtrasCoreMod.MOD_ID, version = McExtrasCoreMod.MOD_VERSION, acceptedMinecraftVersions = "[1.7.10,)", dependencies = "required-after:Forge@[10.13.4.1614,)")
 public class McExtrasCoreMod {
@@ -17,48 +11,5 @@ public class McExtrasCoreMod {
 
     @Mod.Instance(MOD_ID)
     public static McExtrasCoreMod instance;
-
-    private BlockRegistry blockRegistry;
-    private ItemRegistry itemRegistry;
-    private RecipeRegistry recipeRegistry;
-    private WorldGeneratorRegistry worldGeneratorRegistry;
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        if (blockRegistry != null) {
-            blockRegistry.registerBlocks();
-        }
-
-        if (itemRegistry != null) {
-            itemRegistry.registerItems();
-        }
-
-        if (recipeRegistry != null) {
-            recipeRegistry.registerRecipes();
-        }
-    }
-
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        if (worldGeneratorRegistry != null) {
-            worldGeneratorRegistry.registerWorldGenerators();
-        }
-    }
-
-    protected void setBlockRegistry(BlockRegistry blockRegistry) {
-        this.blockRegistry = blockRegistry;
-    }
-
-    protected void setItemRegistry(ItemRegistry itemRegistry) {
-        this.itemRegistry = itemRegistry;
-    }
-
-    protected void setRecipeRegistry(RecipeRegistry recipeRegistry) {
-        this.recipeRegistry = recipeRegistry;
-    }
-
-    protected void setWorldGeneratorRegistry(WorldGeneratorRegistry worldGeneratorRegistry) {
-        this.worldGeneratorRegistry = worldGeneratorRegistry;
-    }
 
 }
--- nether/src/main/java/yg0r2/extras/nether/McExtrasNetherMod.java
+++ nether/src/main/java/yg0r2/extras/nether/McExtrasNetherMod.java
@@ -1,6 +1,8 @@
 package yg0r2.extras.nether;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import yg0r2.extras.core.McExtrasCoreMod;
 import yg0r2.extras.nether.blocks.NetherBlockRegistry;
 import yg0r2.extras.nether.items.NetherItemRegistry;
@@ -8,21 +10,26 @@ import yg0r2.extras.nether.recipes.NetherRecipeRegistry;
 import yg0r2.extras.nether.world.NetherWorldGeneratorRegistry;
 
 @Mod(name = McExtrasNetherMod.MOD_NAME, modid = McExtrasNetherMod.MOD_ID, version = McExtrasNetherMod.MOD_VERSION, dependencies = McExtrasNetherMod.DEPENDENCIES)
-public final class McExtrasNetherMod extends McExtrasCoreMod {
+public final class McExtrasNetherMod {
 
     public static final String MOD_ID = "mc-extras-nether";
     public static final String MOD_NAME = "MC Extras Nether";
     public static final String MOD_VERSION = "@VERSION@";
-    public static final String DEPENDENCIES = "required-after:BuildCraft|Core@" + MOD_VERSION;
+    public static final String DEPENDENCIES = "required-after:" + McExtrasCoreMod.MOD_ID + "@" + MOD_VERSION;
 
     @Mod.Instance(MOD_ID)
     public static McExtrasNetherMod instance;
 
-    public McExtrasNetherMod() {
-        setBlockRegistry(new NetherBlockRegistry());
-        setItemRegistry(new NetherItemRegistry());
-        setRecipeRegistry(new NetherRecipeRegistry());
-        setWorldGeneratorRegistry(new NetherWorldGeneratorRegistry());
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        (new NetherBlockRegistry()).registerBlocks();
+        (new NetherItemRegistry()).registerItems();
+        (new NetherRecipeRegistry()).registerRecipes();
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        (new NetherWorldGeneratorRegistry()).registerWorldGenerators();
     }
 
 }
