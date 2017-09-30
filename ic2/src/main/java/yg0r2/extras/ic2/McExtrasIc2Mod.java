198c4c01bd651a73626bcf4d09228da2ed59ee27
Yg0R2 <yg0r2@yahoo.com>
2017-09-30 14:26:13 +0200

    Add 'MC Extras IC2' mod

---
+++ ic2/src/main/java/yg0r2/extras/ic2/McExtrasIc2Mod.java
@@ -0,0 +1,17 @@
+package yg0r2.extras.ic2;
+
+import cpw.mods.fml.common.Mod;
+import yg0r2.extras.core.McExtrasCoreMod;
+
+@Mod(name = McExtrasIc2Mod.MOD_NAME, modid = McExtrasIc2Mod.MOD_ID, version = McExtrasIc2Mod.MOD_VERSION, dependencies = McExtrasIc2Mod.DEPENDENCIES)
+public class McExtrasIc2Mod {
+
+    public static final String MOD_ID = "mc-extras-ic2";
+    public static final String MOD_NAME = "MC Extras IC2";
+    public static final String MOD_VERSION = "@VERSION@";
+    public static final String DEPENDENCIES = "required-after:" + McExtrasCoreMod.MOD_ID + "@" + MOD_VERSION;
+
+    @Mod.Instance(MOD_ID)
+    public static McExtrasIc2Mod instance;
+
+}
