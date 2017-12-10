7bda809eab92c8a744040bbd5e8f4c0274cf7046
Yg0R2 <yg0r2@yahoo.com>
2017-12-10 17:42:02 +0100

    Forestry: register red/orange/yellow flowers as farable items

--- forestry/src/main/java/yg0r2/extras/forestry/recipes/registry/ForestryRecipeRegistry.java
+++ forestry/src/main/java/yg0r2/extras/forestry/recipes/registry/ForestryRecipeRegistry.java
@@ -4,9 +4,11 @@ import forestry.api.apiculture.FlowerManager;
 import forestry.api.farming.Farmables;
 import forestry.farming.logic.FarmableGenericCrop;
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import yg0r2.extras.api.blocks.McExtrasBlocks;
+import yg0r2.extras.api.items.McExtrasItem;
 import yg0r2.extras.api.items.McExtrasItems;
 import yg0r2.extras.api.recipes.registry.RecipeRegistry;
 
@@ -19,6 +21,15 @@ public class ForestryRecipeRegistry extends RecipeRegistry {
 
         registerFlower(McExtrasBlocks.GLOW_FLOWER, FlowerManager.FlowerTypeNether);
         registerFarmable(McExtrasItems.GLOW_FLOWER_SEED, McExtrasBlocks.GLOW_FLOWER_CROP);
+
+        registerFlower(McExtrasBlocks.ORANGE_FLOWER, FlowerManager.FlowerTypeVanilla);
+        registerFarmable(McExtrasItems.ORANGE_FLOWER_SEED, McExtrasBlocks.ORANGE_FLOWER_CROP);
+
+        registerFlower(Blocks.red_flower, FlowerManager.FlowerTypeVanilla);
+        registerFarmable(McExtrasItems.RED_FLOWER_SEED, Blocks.red_flower);
+
+        registerFlower(Blocks.yellow_flower, FlowerManager.FlowerTypeVanilla);
+        registerFarmable(McExtrasItems.YELLOW_FLOWER_SEED, Blocks.yellow_flower);
     }
 
     private void registerFarmable(Item seedItem, Block cropBlock) {
