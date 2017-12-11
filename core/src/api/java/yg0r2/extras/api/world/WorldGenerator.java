2e3ac15c4fda3ca2a3e1fb0bd8f61431ba67a40d
Yg0R2 <yg0r2@yahoo.com>
2017-12-11 22:43:13 +0100

    Change y coordinate generation for WorldGenerator

--- core/src/api/java/yg0r2/extras/api/world/WorldGenerator.java
+++ core/src/api/java/yg0r2/extras/api/world/WorldGenerator.java
@@ -3,9 +3,9 @@ package yg0r2.extras.api.world;
 import cpw.mods.fml.common.IWorldGenerator;
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldProviderHell;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraftforge.common.MinecraftForge;
-import yg0r2.extras.api.world.McExtrasWorldGenerator;
 
 import java.util.List;
 import java.util.Random;
@@ -30,7 +30,7 @@ abstract class WorldGenerator implements IWorldGenerator {
 
         for (Block block : blocks) {
             int randPosX = chunkX + random.nextInt(16) + 8;
-            int randPosY = random.nextInt(128);
+            int randPosY = getWorldSpecificY(world, random);
             int randPosZ = chunkZ + random.nextInt(16) + 8;
 
             McExtrasWorldGenerator worldGenerator = new McExtrasWorldGenerator(block, numberOfBlocks);
@@ -39,5 +39,13 @@ abstract class WorldGenerator implements IWorldGenerator {
     }
 
     protected abstract boolean allowedToGenerate(World world);
+
+    private int getWorldSpecificY(World world, Random rnd) {
+        if (world.provider instanceof WorldProviderHell) {
+            return rnd.nextInt(100) + 35;
+        }
+
+        return rnd.nextInt(world.getActualHeight() - 72) + 56;
+    }
 
 }
